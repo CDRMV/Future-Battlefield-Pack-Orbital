477c087961f273e48b2d3c32633e0a23bf3aa1f2
CDRMV <46399086+CDRMV@users.noreply.github.com>
2019-12-14 19:59:00 +0100

    Add Seraphim Tech 1 Orbitalengineer

---
+++ units/XSSP0100/XSSP0100_script.lua
@@ -0,0 +1,13 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local SConstructionUnit = import('/lua/seraphimunits.lua').SConstructionUnit
+
+XSSP0100 = Class(SConstructionUnit) {}
+
+TypeClass = XSSP0100
