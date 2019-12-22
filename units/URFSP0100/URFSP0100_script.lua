e11fff02781acaffbdf4a1c01dbcd66c4820f980
CDRMV <46399086+CDRMV@users.noreply.github.com>
2019-12-22 16:04:00 +0100

    Add Cybran Tech 1 Orbitalengineer

---
+++ units/URFSP0100/URFSP0100_script.lua
@@ -0,0 +1,14 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local CConstructionUnit = import('/lua/cybranunits.lua').CConstructionUnit
+
+URFSP0100 = Class(CConstructionUnit) {}
+
+
+TypeClass = URFSP0100
