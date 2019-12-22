b794b04d9c67b7475d335360aaf5d689369dfbd4
CDRMV <46399086+CDRMV@users.noreply.github.com>
2019-12-22 16:05:49 +0100

    Add Cybran Tech 1 Mass and Energy Satellites

---
+++ units/URFOS1101/URFOS1101_script.lua
@@ -0,0 +1,14 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local CAirUnit = import('/lua/cybranunits.lua').CAirUnit
+
+URFOS1101 = Class(CAirUnit) {}
+
+TypeClass = URFOS1101
