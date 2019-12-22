77e8a2ce660735f143830100535dfda4e9594e5d
CDRMV <46399086+CDRMV@users.noreply.github.com>
2019-12-22 16:04:48 +0100

    Add Cybran Tech 1 and 2 Orbitalfactory

---
+++ units/URFOS2000/URFOS2000_script.lua
@@ -0,0 +1,18 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local CAirFactoryUnit = import('/lua/cybranunits.lua').CAirFactoryUnit 
+
+
+URFOS1000 = Class(CAirFactoryUnit) {
+    Weapons = {
+    },	
+}
+
+TypeClass = URFOS1000
