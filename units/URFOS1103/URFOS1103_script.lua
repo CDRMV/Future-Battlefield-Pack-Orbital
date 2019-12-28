3149e769998be4c8c0be1d55dd371975173ddb78
CDRMV <46399086+CDRMV@users.noreply.github.com>
2019-12-28 18:57:39 +0100

    Add Cybran Tech 1 Anti-Spaceship Defense Satellite

---
+++ units/URFOS1103/URFOS1103_script.lua
@@ -0,0 +1,22 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local CAirUnit = import('/lua/cybranunits.lua').CAirUnit
+local CDFParticleCannonWeapon = import('/lua/cybranweapons.lua').CDFParticleCannonWeapon
+
+URFOS1103 = Class(CAirUnit) {
+
+    Weapons = {
+        MainGun = Class(CDFParticleCannonWeapon) {
+            FxMuzzleFlash = {'/effects/emitters/particle_cannon_muzzle_02_emit.bp'},
+        },
+    },
+}
+
+TypeClass = URFOS1103
