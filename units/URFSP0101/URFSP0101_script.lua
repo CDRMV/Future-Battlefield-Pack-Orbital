065e06e8564eab6b97c492f0da116ba2c1cc2797
CDRMV <46399086+CDRMV@users.noreply.github.com>
2019-12-28 18:59:22 +0100

    Add Cybran Tech 1 Light Corvette - Spaceship (V 1.0)

---
+++ units/URFSP0101/URFSP0101_script.lua
@@ -0,0 +1,17 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local CAirUnit = import('/lua/cybranunits.lua').CAirUnit
+
+URFSP0101 = Class(CAirUnit) {
+
+}
+
+
+TypeClass = URFSP0101
