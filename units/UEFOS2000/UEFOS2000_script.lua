70424ab8712d64bcea7ea6051c3b787373f603ad
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-01-01 13:57:23 +0100

    Change UnitIDs for the UEF Orbitalfactories on Tech  2 and 3

---
+++ units/UEFOS2000/UEFOS2000_script.lua
@@ -0,0 +1,23 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local TAirFactoryUnit = import('/lua/terranunits.lua').TAirFactoryUnit 
+local WeaponFile = import('/lua/terranweapons.lua')
+local TDFOrbitalLaserWeapon = WeaponFile.TDFOrbitalLaserWeapon
+local TDFOrbitalLightLaserWeapon = WeaponFile.TDFOrbitalLightLaserWeapon
+
+UEFOS2000 = Class(TAirFactoryUnit) {
+    Weapons = {
+        MainGun = Class(TDFOrbitalLaserWeapon) {},
+        MainGun1 = Class(TDFOrbitalLaserWeapon) {},
+        MainGun2 = Class(TDFOrbitalLightLaserWeapon) {},
+    },	
+}
+
+TypeClass = UEFOS2000
