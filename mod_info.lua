0daab8bf7ef36bc2d335133e6530c4d4bcc6196c
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-03-30 18:41:45 +0200

    Add new Icon + Modinfo

--- mod_info.lua
+++ mod_info.lua
@@ -1,14 +1,14 @@
-name = "Future Battlefield Pack (F.B.P.) - Orbital"
+name = "(F.B.P.) Future Battlefield Pack: Orbital"
 version = 1.0
-copyright = "Copyright © 2017, CDRMV (aka CDR Deadalus)"
+copyright = "Copyright © 2020, CDRMV (aka CDR Deadalus)"
 author = "CDRMV (aka CDR Deadalus)"
-description = "'Future Battlefield Pack Orbital' is a new Unitpack for Forged Alliance, which adds a new Orbitaltechtree for all four Fraktions into the Game."
+description = "'Future Battlefield Pack: Orbital' is a new Unitpack for Forged Alliance, which adds a new Orbitaltechtree for all four Fraktions into the Game."
 url = "http://www.moddb.com/mods/fbp-future-battlefield-pack"
 
-uid = "5t3edt-btz6-9437-h6ui-967gt56fb1"
+uid = "5t3edt-btz6-9437-h6ui-967gt56fa5"
 exclusive = false
 ui_only = false
 selectable = true
-requires = { "5t3edt-btz6-9438-h6ui-967gt56fa1" }
+requires = {}
 icon = "/mods/Future Battlefield Pack Orbital/Icon.png" 
 before = { }
