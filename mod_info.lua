ee53db7cdd61bea9c80eb2261c671d93bfdef1dc
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-04-11 17:02:43 +0200

    Update mod_info.lua

--- mod_info.lua
+++ mod_info.lua
@@ -2,7 +2,7 @@ name = "(F.B.P.) Future Battlefield Pack: Orbital"
 version = 1.0
 copyright = "Copyright © 2020, CDRMV (aka CDR Deadalus)"
 author = "CDRMV (aka CDR Deadalus)"
-description = "'Future Battlefield Pack: Orbital' is a new Unitpack for Forged Alliance, which adds a new Orbitaltechtree for all four Fraktions into the Game."
+description = "'Future Battlefield Pack: Orbital' is a new Unitpack for Forged Alliance, which adds a new Orbitaltechtree for all four Fractions into the Game."
 url = "http://www.moddb.com/mods/fbp-future-battlefield-pack"
 
 uid = "5t3edt-btz6-9437-h6ui-967gt56fa5"
