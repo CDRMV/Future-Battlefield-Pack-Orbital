8fdeede272a989b6ebbfe52c95388eb0a1b56984
CDRMV <46399086+CDRMV@users.noreply.github.com>
2021-06-13 12:36:58 +0200

    Add UEF Experimental Cityship + New Shield - Code in WIP

---
+++ units/UEB0800/UEB0800_Script.lua
@@ -0,0 +1,17 @@
+#****************************************************************************
+#** 
+#**  File     :  /cdimage/units/UEC1101/UEC1101_script.lua 
+#** 
+#** 
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local TLandFactoryUnit = import('/lua/terranunits.lua').TLandFactoryUnit
+local TShieldStructureUnit = import('/lua/terranunits.lua').TShieldStructureUnit
+
+UEB0800 = Class(TLandFactoryUnit) {
+
+}
+
+
+TypeClass = UEB0800
+
