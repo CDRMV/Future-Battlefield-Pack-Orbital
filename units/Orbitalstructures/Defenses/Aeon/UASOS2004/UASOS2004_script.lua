79090a03605295c80dd4323d8d3fe3ae79f0bf56
CDRMV <46399086+CDRMV@users.noreply.github.com>
2021-07-13 16:29:18 +0200

    Add Aeon Orbital Defenses (Still in WIP)

---
+++ units/Orbitalstructures/Defenses/Aeon/UASOS2004/UASOS2004_script.lua
@@ -0,0 +1,16 @@
+#****************************************************************************
+#**
+#**  File     :  /units/XEL0209/XEL0209_script.lua
+#**
+#**  Summary  :  UEF Combat Engineer T2
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local AAirUnit  = import('/lua/aeonunits.lua').AAirUnit 
+
+UASOS2004 = Class(AAirUnit) {
+
+}
+
+TypeClass = UASOS2004
